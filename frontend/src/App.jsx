c787ca1c983f83046d1da1e71e8cacc98b925c61
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-05-15 01:53:04 -0300

    revert: recreating frontend

--- frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './pages/Admin/Admin.module.scss'
-
-function App() {
-  return <div className={styles.app}>Hello World</div>
-}
-
-export default App
--- frontend/src/tests/mocks/mockFetch.ts
+++
@@ -1,9 +0,0 @@
-import mockData from './mockData';
-
-const mockFetch = () => Promise.resolve({
-  status: 200,
-  ok: true,
-  json: () => Promise.resolve(mockData),
-});
-
-export default mockFetch
